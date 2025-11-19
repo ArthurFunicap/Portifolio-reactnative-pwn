bcc49527e203bc6887491c54b9fdc9f076248b3c
ArthurFunicap <arthur.00000849887@unicap.br>
2025-11-18 23:01:02 -0300

    algumas alterações iniciais

---
+++ Portifolio_reactNative/app/(tabs)/index.jsx
@@ -0,0 +1,44 @@
+import { Image } from 'expo-image';
+import { Platform, ScrollView, StyleSheet, View, Text, Pressable } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={{ flex: 1 }}>
+        <View style={styles.container}>
+          <View style={styles.viewText}>
+            <Text style={styles.text}>
+              Bem-vindo
+            </Text>
+          </View>
+          <View style={styles.botao}>
+            <Pressable>
+              <Text>Acessar Portifólio</Text>
+            </Pressable>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'rgba(73, 108, 204, 0.85)'
+  },
+  viewText: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  botao: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
+
+export default App;
